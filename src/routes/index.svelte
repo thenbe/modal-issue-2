de27af2ed9ab08ad4c62b698cd0f09770d2c66b3
nbe <aleisa.naser@gmail.com>
2022-02-27 21:31:04 +0300

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Modal from '$lib/Modal.svelte';
+	let isOpen = false;
+</script>
+
+<button
+	on:click={() => {
+		isOpen = true;
+	}}>Open</button
+>
+
+<!-- This modal behaves as expected -->
+<!-- It dissapears when clicking the navigateAway button -->
+<Modal bind:isOpen />
+
+<!-- ################## -->
+
+<!-- This does not behave as expected -->
+<!-- It does not disappear when clicking the navigateAway button -->
+<!-- don't forget to comment out the first modal before uncommenting the second -->
+<!-- <div>
+	<Modal bind:isOpen />
+</div> -->
